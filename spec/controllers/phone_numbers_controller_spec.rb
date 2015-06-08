a4352582be8d74af47f5381b3ced421036111946
Sally MacNicholas <smacnicholas@gmail.com>
2015-06-08 01:42:49 -0600

    redirect to phone number after adding phone number

--- spec/controllers/phone_numbers_controller_spec.rb
+++ spec/controllers/phone_numbers_controller_spec.rb
@@ -108,6 +108,13 @@ RSpec.describe PhoneNumbersController, type: :controller do
   end
 
   describe "PUT #update" do
+    describe "with valid params" do
+      let(:bob) { Person.create(first_name: 'Bob', last_name: 'Jones') }
+      let(:valid_attributes) { {number: '555-5678', person_id: bob.id} }
+      let(:new_attributes) { {number: 'MyNewString', person_id: bob.id} }
+    end
+
+
     context "with valid params" do
       let(:new_attributes) {
         {number: 'MyNewString', person_id: 2}
@@ -128,9 +135,11 @@ RSpec.describe PhoneNumbersController, type: :controller do
       end
 
       it "redirects to the phone_number" do
+        bob = Person.create(first_name: 'Bob', last_name: 'Jones')
+        valid_attributes = {number: '555-5678', person_id: bob.id}
         phone_number = PhoneNumber.create! valid_attributes
         put :update, {:id => phone_number.to_param, :phone_number => valid_attributes}, valid_session
-        expect(response).to redirect_to(phone_number)
+        expect(response).to redirect_to(bob)
       end
     end
 
